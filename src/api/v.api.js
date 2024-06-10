f0d5c7e6ae6d67d9cfbdd9323588e2d28eeedf53
Javier Alejandro Arteaga Gonzales <132113806+javierarteagagonzales@users.noreply.github.com>
2024-06-10 02:15:50 -0500

    F

--- src/api/v.api.js
+++ src/api/v.api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const getAllData = () => {
-    return axios.get('http://127.0.0.1:8000/sistema/acabado/')
+    return axios.get('https://sistema-web-v.onrender.com/')
 }
