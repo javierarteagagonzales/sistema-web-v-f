f9d3a054b26776a22dd6dece7e5a3d1372b2787b
Javier Alejandro Arteaga Gonzales <132113806+javierarteagagonzales@users.noreply.github.com>
2024-06-14 11:24:44 -0500

    j

--- src/components/nos.jsx
+++ src/components/nos.jsx
@@ -7,13 +7,13 @@ const Nos = () => {
       <section>
         <h3>Nuestra Misión</h3>
         <p>
-          En Vitex nos dedicamos apasionadamente a la confección de prendas de la más alta calidad, destinadas a mercados exigentes a nivel internacional.
+        Nuestra misión es establecer un estándar ejemplar en la industria textil y de confección al combinar la excelencia en la calidad, la flexibilidad en la producción y el cumplimiento riguroso de los plazos, todo ello respaldado por un compromiso inquebrantable con la generación de empleo y el desarrollo socioeconómico de nuestro país.
         </p>
       </section>
       <section>
         <h3>Nuestra Visión</h3>
         <p>
-          En un plazo de cinco años, Vitex se visualiza como el líder indiscutible en la mente de nuestros compradores...
+        En un plazo de cinco años, Vitex se visualiza como el líder indiscutible en la mente de nuestros compradores. Siendo elegidos por nuestra capacidad excepcional para entender, atender y superar las expectativas de nuestros clientes en términos de tiempos de entrega, flexibilidad y calidad de producto , contribuyendo de manera significativa al crecimiento del mercado textil y de confecciones de exportación peruano.
         </p>
       </section>
       <section>
