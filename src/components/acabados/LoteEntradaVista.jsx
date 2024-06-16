cc25addbdec71f9f09727f9d3ffe31e22a76e09b
Javier Alejandro Arteaga Gonzales <132113806+javierarteagagonzales@users.noreply.github.com>
2024-06-16 03:40:42 -0500

    er

--- src/components/acabados/LoteEntradaVista.jsx
+++ src/components/acabados/LoteEntradaVista.jsx
@@ -13,6 +13,32 @@ import {
     CircularProgress,
 } from '@mui/material';
 
+import { tableCellClasses } from "@mui/material/TableCell";
+
+//estilos
+import { styled } from "@mui/material/styles";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 10,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+
 const LoteEntradaVista = () => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -43,31 +69,28 @@ const LoteEntradaVista = () => {
 
     return (
         <Container>
-            <Typography variant="h4" gutterBottom>
-                Vista Lote Entrada
-            </Typography>
             <TableContainer component={Paper}>
                 <Table>
                     <TableHead>
-                        <TableRow>
-                            <TableCell>ID Entrada</TableCell>
-                            <TableCell>Fecha Entrada</TableCell>
-                            <TableCell>ID Tipo Lote</TableCell>
-                            <TableCell>Cantidad</TableCell>
-                            <TableCell>ID Dim Confección</TableCell>
-                            <TableCell>ID Guía Confección</TableCell>
-                        </TableRow>
+                        <StyledTableRow>
+                            <StyledTableCell>ID Entrada</StyledTableCell>
+                            <StyledTableCell>Fecha Entrada</StyledTableCell>
+                            <StyledTableCell>ID Tipo Lote</StyledTableCell>
+                            <StyledTableCell>Cantidad</StyledTableCell>
+                            <StyledTableCell>ID Dim Confección</StyledTableCell>
+                            <StyledTableCell>ID Guía Confección</StyledTableCell>
+                        </StyledTableRow>
                     </TableHead>
                     <TableBody>
                         {data.map(item => (
-                            <TableRow key={item.id_entrada}>
-                                <TableCell>{item.id_entrada}</TableCell>
-                                <TableCell>{new Date(item.fecha_entrada).toLocaleString()}</TableCell>
-                                <TableCell>{item.id_tipo_lote}</TableCell>
-                                <TableCell>{item.cantidad}</TableCell>
-                                <TableCell>{item.id_dim_confeccion}</TableCell>
-                                <TableCell>{item.id_guia_confeccion}</TableCell>
-                            </TableRow>
+                            <StyledTableRow key={item.id_entrada}>
+                                <StyledTableCell>{item.id_entrada}</StyledTableCell>
+                                <StyledTableCell>{new Date(item.fecha_entrada).toLocaleString()}</StyledTableCell>
+                                <StyledTableCell>{item.id_tipo_lote}</StyledTableCell>
+                                <StyledTableCell>{item.cantidad}</StyledTableCell>
+                                <StyledTableCell>{item.id_dim_confeccion}</StyledTableCell>
+                                <StyledTableCell>{item.id_guia_confeccion}</StyledTableCell>
+                            </StyledTableRow>
                         ))}
                     </TableBody>
                 </Table>
