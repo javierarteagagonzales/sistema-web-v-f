f5090bafb6d7416d86a24a4d1203729dfa903ca2
Javier Alejandro Arteaga Gonzales <132113806+javierarteagagonzales@users.noreply.github.com>
2024-06-21 01:32:29 -0500

    templates

---
+++ src/components/almacencentral/conprovee.jsx
@@ -0,0 +1,63 @@
+// consulta de lotes
+import React from "react";
+
+// tablas
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+
+//estilos
+import { styled } from "@mui/material/styles";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 10,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+export default function ConsultaProveedor() {
+
+
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ maxWidth: 450 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <StyledTableRow>
+            <StyledTableCell>id_materia_prima</StyledTableCell>
+            <StyledTableCell>cantidad_lotes</StyledTableCell>
+          </StyledTableRow>
+        </TableHead>
+        <TableBody>
+
+            <StyledTableRow >
+              <StyledTableCell>--</StyledTableCell>
+              <StyledTableCell>--</StyledTableCell>
+
+
+            </StyledTableRow>
+
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/pages/almacencentral/estado.jsx
+++ src/pages/almacencentral/estado.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import Sidenav from '../../components/Sidenav'
 import Navbar from '../../components/Navbar'
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import ConsultaCentral from '../../components/almacencentral/consulta'
+import ConsultaLotesFechas from '../../components/almacencentral/conlotesf';
+import ConsultaProveedor from '../../components/almacencentral/conprovee';
+import ConsultaLotesActividad from '../../components/almacencentral/conlotesact';
+import ConsultaLotesEstado from '../../components/almacencentral/conlotesest';
+import ConsultaLotesEntrada from '../../components/almacencentral/conlotesent';
+import ConsultaLotesSalida from '../../components/almacencentral/conlotessal';
 
 export default function EstadoPage() {
   return (
@@ -14,13 +20,49 @@ export default function EstadoPage() {
       
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
        <h1>Área de Almacén Central</h1> 
-        <Typography paragraph>
-          resumen
-        </Typography>
-      </Box>
-
-    </Box>
+       <Box>
+        <h3>
+          Consulta de lotes por estado
+        </h3>
+        
+      <div><ConsultaCentral /></div>
+      <h3>
+          Consulta de lotes entre fechas
+        </h3>
+        
+      <div><ConsultaLotesFechas /></div>
       
+      <h3>
+          Consulta por proveedor
+        </h3>
+        
+      <div><ConsultaProveedor /></div>
+      <h3>
+          Cantidad de lotes por actividad
+        </h3>
+        
+      <div><ConsultaLotesActividad /></div>
+      <h3>
+          Cantidad de lotes por estado
+        </h3>
+        
+      <div><ConsultaLotesEstado /></div>
+      <h3>
+          Lotes de entrada en una fecha
+        </h3>
+        
+      <div><ConsultaLotesEntrada /></div>
+      <h3>
+          Lotes de salida en una fecha
+        </h3>
+        
+      <div><ConsultaLotesSalida /></div>
+      
+      </Box>
+      </Box>
+      
+    </Box>
+    
     
       
     </>
