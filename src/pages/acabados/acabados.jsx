955c8eeae93c6640d4ef401b77b099deecf0b953
Javier Alejandro Arteaga Gonzales <132113806+javierarteagagonzales@users.noreply.github.com>
2024-06-25 03:53:29 -0500

    v

--- src/pages/acabados/acabados.jsx
+++ src/pages/acabados/acabados.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Box from "@mui/material/Box";
-//import Typography from "@mui/material/Typography";
 import { Grid, MenuItem, Select, FormControl, InputLabel } from "@mui/material";
 import { DataGrid } from '@mui/x-data-grid';
 import Sidenav from "../../components/Sidenav";
 import Navbar from "../../components/Navbar";
 import Navigation from "../../components/Navigation";
+
 export default function AcabadoPage() {
   const [empleados, setEmpleados] = useState([]);
   const [selectedEmpleado, setSelectedEmpleado] = useState('');
@@ -19,9 +19,12 @@ export default function AcabadoPage() {
 
   useEffect(() => {
     if (selectedEmpleado) {
-      fetch(`https://sistema-web-v.onrender.com/sistema/?empleado=${selectedEmpleado}`)
+      fetch(`https://sistema-web-v.onrender.com/sistema/datosa/?empleado=${selectedEmpleado}`)
         .then(response => response.json())
-        .then(data => setDatos(data));
+        .then(data => {
+          const dataWithIds = data.map((row, index) => ({ ...row, id: row.ID_Caja || index }));
+          setDatos(dataWithIds);
+        });
     }
   }, [selectedEmpleado]);
 
@@ -32,62 +35,55 @@ export default function AcabadoPage() {
   const columns = [
     { field: 'ID_Caja', headerName: 'ID Caja', maxWidth: 80 },
     { field: 'nombre', headerName: 'Empleado', maxWidth: 80 },
-    { field: 'cantidad', headerName: 'Cantidad', maxWidth: 80},
+    { field: 'cantidad', headerName: 'Cantidad', maxWidth: 80 },
     { field: 'ID_guia', headerName: 'ID Guía', maxWidth: 80 },
     { field: 'tipo_prenda', headerName: 'Tipo Prenda', maxWidth: 80 },
     { field: 'estilo_prenda', headerName: 'Estilo Prenda', maxWidth: 80 },
     { field: 'talla', headerName: 'Talla', maxWidth: 70 },
     { field: 'genero', headerName: 'Género', maxWidth: 60 },
-    { field: 'ml', headerName: 'ml', maxWidth: 40},
+    { field: 'ml', headerName: 'ml', maxWidth: 40 },
     { field: 'mh', headerName: 'mh', maxWidth: 40 },
     { field: 'mp', headerName: 'mp', maxWidth: 40 },
     { field: 'mm', headerName: 'mm', maxWidth: 40 },
     { field: 'mc', headerName: 'mc', maxWidth: 40 },
-    { field: 'mca', headerName: 'mc', maxWidth: 40 },
+    { field: 'mca', headerName: 'mca', maxWidth: 40 },
     { field: 'mmu', headerName: 'mmu', maxWidth: 40 },
   ];
 
   return (
     <>
-
-<Navbar />
+      <Navbar />
       <Box height={30} />
       <Box sx={{ display: "flex" }}>
         <Sidenav />
-
         <Box component="main" sx={{ flexGrow: 1, p: 1 }}>
-      <h1>Área de Acabados</h1>
-              
-                <Navigation />{" "}
-             
-              <Grid container spacing={3} alignItems="center" sx={{ padding: 1 }}>
-              <Grid item xs={3}>
-        <FormControl fullWidth>
-          <InputLabel>Operario</InputLabel>
-          <Select value={selectedEmpleado} onChange={handleEmpleadoChange}>
-            {empleados.map((empleado) => (
-              <MenuItem key={empleado.nombre} value={empleado.nombre}>
-                {empleado.nombre}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        </Grid></Grid>
-            
-        <Box mt={3}  width={1000} >
-        <div style={{ height: 400, width: '100%' }}>
-          <DataGrid 
-          rows={datos} 
-          columns={columns} 
-          pageSize={5} />
-          </div>
+          <h1>Área de Acabados</h1>
+          <Navigation />
+          <Grid container spacing={3} alignItems="center" sx={{ padding: 1 }}>
+            <Grid item xs={3}>
+              <FormControl fullWidth>
+                <InputLabel>Operario</InputLabel>
+                <Select value={selectedEmpleado} onChange={handleEmpleadoChange}>
+                  {empleados.map((empleado) => (
+                    <MenuItem key={empleado.nombre} value={empleado.nombre}>
+                      {empleado.nombre}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+          <Box mt={3} width={1000}>
+            <div style={{ height: 400, width: '100%' }}>
+              <DataGrid 
+                rows={datos} 
+                columns={columns} 
+                pageSize={5}
+              />
+            </div>
+          </Box>
         </Box>
-        
-
       </Box>
-
-      </Box>
-
     </>
   );
 }
